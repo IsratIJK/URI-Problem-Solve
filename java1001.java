b01b9ef4cc9a49fd2b6c04f72c039903b4d6a5e5
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-04 20:20:03 +0600

    Two java code added

---
+++ java1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a+b;
+        System.out.print("X = "+c+"\n");
+
+    }
+    
+}
