e0bd612f66a66258d6b59adaf76d6df912e27c00
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-22 22:15:32 +0600

    java 1002 added

---
+++ 1002.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double A,R,pi;
+        R=input.nextInt();
+        
+        pi=3.14159;
+        A=pi*R*R;
+        System.out.println(A);
+
+    }
+    
+}
