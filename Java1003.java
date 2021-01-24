eaf05e038236995f3f79305a26e1ddbd840f144e
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-24 15:44:25 +0600

    java 1003 added

---
+++ Java1003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Java1003 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int A,B;
+        A=input.nextInt();
+        B=input.nextInt();
+        
+    
+        System.out.print("SOMA = "+(A+B)+"\n");
+
+    }
+    
+}
