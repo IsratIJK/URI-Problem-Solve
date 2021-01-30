db8a6cd5bd3351edef9a804b49425bcb9cbe6bf2
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-30 22:26:06 +0600

    30.1.2021

---
+++ 1004.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int A,B,PROD;
+        A = input.nextInt();
+        B = input.nextInt();
+        PROD=A*B;
+        System.out.print("PROD = "+PROD+"\n");
+    }
+}
+
+ 
